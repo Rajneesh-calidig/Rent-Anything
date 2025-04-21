ea9a578ef991af51e7d97de686d4a309b787496d
Rajneesh-calidig <rajneesh@calidig.com>
2025-04-21 19:53:43 +0530

    ready to deploye

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 // app.use(cookies());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://rent-anything-frontend.vercel.app/',
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials:true
 }
