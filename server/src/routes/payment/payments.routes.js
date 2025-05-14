8c632d4f7b544ffae39903e3edad2994de31126c
Rajneesh-calidig <rajneesh@calidig.com>
2025-05-14 18:25:17 +0530

    Update payments.routes.js

--- server/src/routes/payment/payments.routes.js
+++ server/src/routes/payment/payments.routes.js
@@ -1,11 +1,10 @@
 import express from 'express';
 
 const router = express.Router();
-import {createOrderByStripe,verifyPayment,createListerAccount,listerPayout,updateConnectedAccount,getConnectedAccountBalance,getUserTransection} from "../../controllers/payments.controller.js"
+import {createOrderByStripe,createListerAccount,listerPayout,updateConnectedAccount,getConnectedAccountBalance,getUserTransection} from "../../controllers/payments.controller.js"
 
 router.post('/create-checkout-session',createOrderByStripe)
 // router.post('/webhook', express.raw({ type: 'application/json' }),stripeWebhook)
-// router.post("/verify-payment",verifyPayment)
 router.post('/create-linked-account',createListerAccount)
 router.post('/update-account/:accountId',updateConnectedAccount)
 router.post('/lister-payout',listerPayout)
